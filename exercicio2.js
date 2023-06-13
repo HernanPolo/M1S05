458299eba80f12fde66d72db38b30e8ada531410
Hernan Polo <hernan.polo1985@gmail.com>
2023-06-13 17:13:28 -0300

    Tradução de cores finalizado

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,21 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+var cor="black"
+switch (cor){
+    case "red":
+     console.log("vermelho")
+     break;
+    case "green":
+     console.log("verde") 
+     break;
+    case "yellow":
+     console.log("amarelo")
+     break;
+    case "black":
+     console.log("preto")
+     break
+    case "white":
+     console.log("branco")
+}
