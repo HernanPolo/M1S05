877a231121c3d5225abc8de9f8eeb78a13840c40
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-23 20:44:45 -0300

    sec fibbonacci

--- exercicio4.js
+++ exercicio4.js
@@ -6,3 +6,13 @@
  * 0, 1, 1, 2, 3, 5, 8, 13, ...
  * Utiliza uma estrutura de repetição (while) para gerar a sequência.
  */
+function secFibbonacci(numero){
+     let fib=[0,1]
+     let i = 2;
+     while(fib[i-1]+fib[i-2]<= numero){
+         fib[i]=fib[i-1]+fib[i-2];
+         i++;
+     }
+     console.log(fib)
+ }
+ secFibbonacci(55)
