4731abf8120d58841af8d4ec378be5ae03cf1369
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-23 20:45:17 -0300

    for if

--- exercicio5.js
+++ exercicio5.js
@@ -1,6 +1,19 @@
 /**
  * [M1S05] Ex 5 - Verificar palavras em um array
  *
- * Escreve um programa que percorre um array de palavras e exibe todas as palavras que possuem
- * mais de 5 caracteres. Utiliza a estrutura de repetição for of para percorrer o array.
+ * Escreve um programa que percorre um array de palavras e
+ *  exibe todas as palavras que possuem
+ * mais de 6 caracteres. Utiliza a estrutura de repetição
+ *  for of para percorrer o array.
  */
+
+let nomesAleato=['Eva','Pedro','Bruno','Jessica','Luiz',
+'Jose','Lia','Carla','Ana','Sol','Flor',
+]
+ for ( var nome of nomesAleato){
+   if (nome.length >= 5 ){
+    console.log(nome)
+   }
+ }
+
+ 
