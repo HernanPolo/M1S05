0719acf9aadfd2c4d11592e061029f4f25d7f365
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-23 20:47:07 -0300

    array de objetos

--- exercicio6.js
+++ exercicio6.js
@@ -5,3 +5,20 @@
  * e o preço de cada produto. Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
+let mercadoria= [
+prod1={
+    nome:'laranja',
+    preco: 5.52},
+prod2={ 
+    nome:'maça',
+    preco:2.33},
+ prod3={ 
+    nome:'morango',
+    preco:6.50},
+prod4={ 
+    nome:'abacaxi',
+    preco:7},   
+]
+console.log(mercadoria)
+console.log(`o a quantidade de  produtos é ${mercadoria.length}.`)
+console.log(`O valor total de todos os produtos é R$ ${(prod1.preco+prod2.preco+prod3.preco+prod4.preco)} .`)
